2d76247f432ead00f188ef3ba1ea51a80bf71aa3
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2019-02-02 01:42:41 +0300

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+#!creating Functions
+
+def say_welcome():
+    print("Hello There")
+
+say_welcome() #!This code calls the function
+
+#!Adding Parameters
+def say_hey(name):
+    print("Hello " + name)
+
+say_hey("George")
+say_hey("Milly")
+
+#!Go ahead and Test it Out!!!!
